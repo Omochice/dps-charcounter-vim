ddb019e6680c6ab40e96bd2954d5ed7da2c7b759
GitHub <noreply@github.com>
2023-03-17 01:39:40 +0000

    :package: update deno dependencies

--- denops/charcounter/deps.ts
+++ denops/charcounter/deps.ts
@@ -1,12 +1,12 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.3.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.3.0/helper/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts";
-export * as popup from "https://deno.land/x/denops_popup@v2.0.1/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.1.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v4.1.0/helper/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v4.1.0/autocmd/mod.ts";
+export * as popup from "https://deno.land/x/denops_popup@v2.2.0/mod.ts";
 export {
   ensureNumber,
   ensureArray,
   ensureString,
   isString,
-} from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+} from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
 
 
